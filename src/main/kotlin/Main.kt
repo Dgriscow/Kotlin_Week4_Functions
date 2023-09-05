ca10c9a351def43d27b542ce70c67e8b9fa75368
dgriscow <dgrischow@students.mchenry.edu>
2023-09-05 15:57:18 -0500

    completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,69 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun calculateSquareArea(){
+    //get one of the sides and calculate the area
+    println("What is the Length of One Side?")
+    var sideInput = readln().toDouble()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    var area = sideInput * sideInput
+    println("The Total Area of the Square is: $area")
+
+}
+
+fun calculateTriangleArea(){
+    //get each side first as a seperate variable
+    println("What is the length of the base?")
+    var base = readln().toDouble()
+
+    println("What is the height of the triangle?")
+    var height = readln().toDouble()
+
+    //calculate the area of the triangle
+    var area = base * height
+    area = area * 0.5
+
+    println("The Area of the Triangle is: $area")
+
+}
+ fun calculateParallelogram(){
+     println("What is the Length The Base?")
+     val base = readln().toDouble()
+
+     println("What is the Height?")
+     val height = readln().toDouble()
+
+     val area = base * height
+     println("The Total Area of the Parallelogram is: $area")
+
+ }
+
+fun calculateRectangle(){
+    println("What is the Length The Base?")
+    val base = readln().toDouble()
+
+    println("What is the Height Of the Rectangle?")
+    val height = readln().toDouble()
+
+    val area = base * height
+    println("The Total Area of the Rectangle is: $area")
+
+}
+
+fun main() {
+    println("What Shape Area would you Like To Calculate?\n" +
+            "1: Square\n2: Triangle\n3: Parallelogram\n4: Rectangle\n" +
+            "Type A Number to Choose Your Calculator")
+
+
+    var userChoice = readln().toInt()
+
+    when (userChoice){
+        1 ->calculateSquareArea()
+        2 ->calculateTriangleArea()
+        3->calculateParallelogram()
+        4->calculateRectangle()
+
+
+    }
+
+
+
 }
